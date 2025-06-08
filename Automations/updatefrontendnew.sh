1a68d72461e168a6a1a32a87b70148234dd081c8
rajat892 <kumar.rishu892@gmail.com>
2025-06-08 11:26:56 +0000

    updated instance-id

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
+INSTANCE_ID="gke-test-test-pool-f2664938-w56q"
 
 # Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-
+#ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+ipv4_address=$(gcloud compute instances describe $INSTANCE_ID --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone="us-central1-c")
 # Path to the .env file
 file_to_find="../frontend/.env.docker"
 
